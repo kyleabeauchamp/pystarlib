0693511458b348f5733fda2367ddc9443a1907c1
jurgenfd <jurgenfd@6716320e-cb4c-0410-9e63-1b2342504021>
2007-01-09 22:10:14 +0000

    Initial revision

---
+++ src/STAR/Utils.py
@@ -0,0 +1,57 @@
+"""
+Just a few utilities that can be of more general use.
+"""
+
+__author__    = "$Author$"
+___revision__ = "$Revision$"
+___date__     = "$Date$"
+
+"""
+$Log$
+Revision 1.1  2007/01/09 22:10:15  jurgenfd
+Initial revision
+
+Revision 1.1  2007/01/08 20:49:41  jurgen
+Merged improvements from Wim Vranken (EBI) back in.
+
+Revision 1.1.1.1  2001/11/02 20:16:40  jurgen
+Initial package capable of read/write access to STAR files without nested loops
+
+"""
+
+class Lister:
+    """Example from 'Learning Python from O'Reilly publisher'"""
+    def __repr__(self):
+        return ("<Instance of %s, address %s:\n%s>" %
+           (self.__class__.__name__, id(self), self.attrnames()))
+
+    def attrnames(self):
+        result=''
+        keys = self.__dict__.keys()
+        keys.sort()
+        for attr in keys:
+            if attr[:2] == "__":
+                result = result + "\tname %s=<built-in>\n" % attr
+            else:
+                result = result + "\tname %s=%s\n" % (attr, self.__dict__[attr])
+        return result        
+
+
+"""
+A fast transposing algorithm from the python mailing list
+Used in TagTable.
+"""
+def transpose ( matrix ):
+    if len( matrix ) < 1:
+        print 'ERROR: trying to transpose an empty matrix'
+        return 1
+    elif len( matrix ) == 1:
+        if len(matrix[0]) == 0:
+            print 'ERROR: trying to transpose an empty matrix, shape would be lost'
+            print 'ERROR: [[]] would become []'
+            return 1
+        else:
+            return map( lambda y : (y,), matrix[0] )
+    else:
+        return apply( map, [None,] + list(matrix) )
+
