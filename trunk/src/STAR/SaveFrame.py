6bb16905eb024e03f41cd19261d9b277748cedd9
jurgenfd <jurgenfd@6716320e-cb4c-0410-9e63-1b2342504021>
2007-01-09 22:10:14 +0000

    Initial revision

---
+++ trunk/src/STAR/SaveFrame.py
@@ -0,0 +1,93 @@
+"""
+Classes for dealing with STAR syntax
+"""
+
+__author__    = "$Author$"
+___revision__ = "$Revision$"
+___date__     = "$Date$"
+
+"""
+Revision 1.1  2003/07/01 12:56:18  wfv20
+Jurgen Doreleijers modified Python nmrStar reader. Bug fixes and additions (notably for reading nmrView star files) have been made.
+
+Revision 1.1.1.1  2001/11/02 20:16:40  jurgen
+Initial package capable of read/write access to STAR files without nested loops
+
+"""
+## Standard modules
+import re
+## BMRB modules
+from TagTable       import *
+from Utils          import *
+
+"""
+Saveframe class
+"""
+class SaveFrame (Lister):
+    def __init__( self,
+                  title     = 'general_sf_title',
+                  tagtables = None,
+                  text      = '',
+                  verbosity = 2,
+                  comment = ''):
+        self.title      = title
+        
+        # Modified tagtables initialization so list references
+        # are not carried through (Wim 14/07/2002)
+        self.tagtables = tagtables
+        
+        if self.tagtables == None:
+          self.tagtables  = []
+          
+        self.text       = text
+        self.verbosity  = verbosity
+        self.comment = comment          # Comment attribute added to node (Wim 2003/08/05)
+        
+    "Returns the STAR text representation"
+    def star_text (self,
+                   flavor = 'NMR-STAR'
+                   ):
+        str = "\n"
+        str = str + 'save_%s\n' % self.title
+        
+        for tagtable in self.tagtables:
+            str = str + tagtable.star_text( flavor=flavor )
+            
+        str = str + '\nsave_\n'
+        return str
+    
+    "Simple checks on integrity"
+    def check_integrity( self,  recursive = 1  ):
+        if recursive:
+            for tagtable in self.tagtables:
+                if tagtable.check_integrity():
+                    print "ERROR: integrity check failed for tagtable"
+                    return 1
+        if self.verbosity >= 9:
+            print 'Checked integrity of SaveFrame(%2s tagtables, recurs.=%s)  : OK [%s]' % (
+                len(self.tagtables), recursive, self.title )
+                
+    """
+    Or print Warning and return None
+    """
+    def getSaveFrameCategory(self, ):
+        possibleTagNamesSFCategory = [ '_Saveframe_category',  # 2.1
+                                       '.Sf_category' ]        # 3
+        if not self.tagtables:
+            print "WARNING: no tagtable found in Saveframe"
+            return None
+        
+        tT = self.tagtables[0] # assumed 0
+        if not tT.tagvalues[0]: # assumed 0
+            print "WARNING: empty tagtable found in Saveframe"
+            return None
+        found = 0
+        for possi in possibleTagNamesSFCategory:
+            if tT.tagnames[0].endswith(possi):
+                found = 1
+        if not found:
+            print "WARNING: first tag doesn't look like a Sf_category; taking value anyway"
+            
+        return tT.tagvalues[0][0]
+
+        
---
+++ trunk/src/STAR/TextTest.py
@@ -0,0 +1,39 @@
+"""Unit test 
+"""
+from STAR.Text import *
+from unittest import TestCase
+import unittest
+
+
+class AllChecks(TestCase):
+    def test(self):
+        """STAR Text Checks"""
+        textExpectedAfterCollapse = ';<eol-string>mmy xie<eol-string>;\n_Test'
+        text = """;
+mmy xie
+;
+_Test"""
+        textCollapsed = semicolon_block_collapse( text )
+        self.assertEqual( textExpectedAfterCollapse, textCollapsed)
+
+        value, pos = tag_value_quoted_parse( textCollapsed, 0 )
+        valueExpected = '\nmmy xie\n'
+        posExpected = 34
+        self.assertEqual( valueExpected, value )
+        self.assertEqual( posExpected, pos)
+        
+        t2 = """
+; my text ## is no comment
+;
+## comment 1
+value ## comment 2
+"""
+        t2noComment = """
+; my text ## is no comment
+;
+value
+"""
+        self.assertEqual(t2noComment,comments_strip( t2 ))
+
+if __name__ == "__main__":
+    unittest.main()
